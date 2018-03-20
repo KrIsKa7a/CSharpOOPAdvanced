24f2499fa0ca355a27c192da4cda792212f1d3fb
KrIsKa7a <kristiqn.ivanov@abv.bg>
2018-03-20 21:36:45 +0200

    Upload SOLID-Lab

---
+++ 01. CSharp-OOP-Advanced-SOLID-Lab/P01.Stream_Progress/IStreamProgressable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.Stream_Progress
+{
+    public interface IStreamProgressable
+    {
+        int Length { get; }
+
+        int BytesSent { get; }
+    }
+}
---
+++ 01. CSharp-OOP-Advanced-SOLID-Lab/P03.Detail_Printer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03.DetailPrinter
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
